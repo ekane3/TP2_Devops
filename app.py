1f2ff5ca64cb54ea6feb7884c4dae07d9c84a792
EKANE <ekaneekane3@gmail.com>
2022-06-17 13:08:50 +0200

    [UPDATE] update get weather method

--- app.py
+++ app.py
@@ -4,22 +4,21 @@ import os
 
 from flask import Flask, render_template, request
 
-api_key = os.environ.get('API_KEY')
 app = Flask(__name__)
 
 # create a function that returns the weather for a specific location using env lat and lon
-def get_weather(lat, lon, api_key):
-    url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
-    response = requests.get(url)
-    data = json.loads(response.text)
-    return data
+@app.route('/')
+def index( ):
+    args = request.args
+    
+    lat = args['lat']
+    lon = args['lon']
+    api_key = os.environ['API_KEY']
 
-@app.route('/<lat>/<lon>')
-def index(lat, lon, api_key ):
-    weather = get_weather(lat, lon, api_key)
-    #return render_template('index.html', weather=weather) #hh
-    return "<p><b>Weather !</b></p><br><p>%s</p>" % weather
+    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid="{api_key}"').text
+    
+    return f"{res}\n"
 
 if __name__ == "__main__":
     #print(get_weather(lat, lon, api_key))
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8083,debug=True)
